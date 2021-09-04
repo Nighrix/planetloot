8aba147fab310070c361c3bac5fde6608054e040
Nighrix <87576288+Nighrix@users.noreply.github.com>
2021-09-05 01:10:44 +0200

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,26 +1,26 @@
 module.exports = {
 "0": {
-  "name:" "HorseWithHats #0",
+  "name:" "Mister Giye",
 "birthday": "10-18"
 },
 "1": {
-  "name:" "HorseWithHats #1",
+  "name:" "Mister Giye",
 "birthday": "10-18"
 },
 "2": {
-  "name:" "HorseWithHats #2",
+  "name:" "Mister Giye",
 "birthday": "10-18"
 },
 "3": {
-  "name:" "HorseWithHats #3",
+  "name:" "Mister Giye",
 "birthday": "10-18"
 },
 "4": {
-  "name:" "HorseWithHats #4",
+  "name:" "Mister Giye",
 "birthday": "10-18"
 },
 "5": {
-  "name:" "HorseWithHats #5",
+  "name:" "Mister Giye",
 "birthday": "10-18"
 }
 }
