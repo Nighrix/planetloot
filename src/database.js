a645d577555240567a38584bf286925440ea1793
Nighrix <87576288+Nighrix@users.noreply.github.com>
2021-09-04 23:50:14 +0200

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,26 +1,26 @@
 module.exports = {
   "0": {
     "name": "Chunky Monkey3",
-    "birthday": "3-18"
+    //"birthday": "3-18"
   },
   "1": {
     "name": "Chunky Monkey1",
-    "birthday": "10-18"
+    //"birthday": "10-18"
   },
   "2": {
     "name": "Flerp Derp",
-    "birthday": "01-08"
+   // "birthday": "01-08"
   },
   "3": {
     "name": "Buttle McFuddle",
-    "birthday": "01-22"
+   // "birthday": "01-22"
   },
   "4": {
     "name": "Quiero Pancakes",
-    "birthday": "03-04"
+   // "birthday": "03-04"
   },
   "5": {
     "name": "Guino the Penguin",
-    "birthday": "03-09"
+   // "birthday": "03-09"
   }
 }
