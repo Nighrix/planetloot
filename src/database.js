fcb2a7368dd826dd11f3b009b2cd71db50f8c7fd
Nighrix <87576288+Nighrix@users.noreply.github.com>
2021-09-05 01:21:52 +0200

    Update database.js

--- src/database.js
+++ src/database.js
@@ -22,5 +22,81 @@ module.exports = {
 "5": {
   "name": "HorseWithHats #5",
 "birthday": "10-18"
+},
+"6": {
+  "name": "HorseWithHats #6",
+"birthday": "10-18"
+},
+"7": {
+  "name": "HorseWithHats #7",
+"birthday": "10-18"
+},
+"8": {
+  "name": "HorseWithHats #8",
+"birthday": "10-18"
+},
+"9": {
+  "name": "HorseWithHats #9",
+"birthday": "10-18"
+},
+"10": {
+  "name": "HorseWithHats #10",
+"birthday": "10-18"
+},
+"11": {
+  "name": "HorseWithHats #11",
+"birthday": "10-18"
+},
+"12": {
+  "name": "HorseWithHats #12",
+"birthday": "10-18"
+},
+"13": {
+  "name": "HorseWithHats #13",
+"birthday": "10-18"
+},
+"14": {
+  "name": "HorseWithHats #14",
+"birthday": "10-18"
+},
+"15": {
+  "name": "HorseWithHats #15",
+"birthday": "10-18"
+},
+"16": {
+  "name": "HorseWithHats #16",
+"birthday": "10-18"
+},
+"17": {
+  "name": "HorseWithHats #17",
+"birthday": "10-18"
+},
+"18": {
+  "name": "HorseWithHats #18",
+"birthday": "10-18"
+},
+"19": {
+  "name": "HorseWithHats #19",
+"birthday": "10-18"
+},
+"20": {
+  "name": "HorseWithHats #20",
+"birthday": "10-18"
+},
+"21": {
+  "name": "HorseWithHats #21",
+"birthday": "10-18"
+},
+"22": {
+  "name": "HorseWithHats #22",
+"birthday": "10-18"
+},
+"23": {
+  "name": "HorseWithHats #23",
+"birthday": "10-18"
+},
+"24": {
+  "name": "HorseWithHats #24",
+"birthday": "10-18"
 }
 }
