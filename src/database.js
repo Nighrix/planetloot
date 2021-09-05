b761b173f0620cad2e3de08a6de6d9f7d1ace30c
Nighrix <87576288+Nighrix@users.noreply.github.com>
2021-09-05 09:55:51 +0200

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,102 +1,1026 @@
 module.exports = {
 "0": {
   "name": "HorseWithHats #0",
-"birthday": "10-18"
+"color": "brown"
 },
 "1": {
   "name": "HorseWithHats #1",
-"birthday": "10-18"
+"color": "brown"
 },
 "2": {
   "name": "HorseWithHats #2",
-"birthday": "10-18"
+"color": "brown"
 },
 "3": {
   "name": "HorseWithHats #3",
-"birthday": "10-18"
+"color": "brown"
 },
 "4": {
   "name": "HorseWithHats #4",
-"birthday": "10-18"
+"color": "brown"
 },
 "5": {
   "name": "HorseWithHats #5",
-"birthday": "10-18"
+"color": "brown"
 },
 "6": {
   "name": "HorseWithHats #6",
-"birthday": "10-18"
+"color": "brown"
 },
 "7": {
   "name": "HorseWithHats #7",
-"birthday": "10-18"
+"color": "brown"
 },
 "8": {
   "name": "HorseWithHats #8",
-"birthday": "10-18"
+"color": "brown"
 },
 "9": {
   "name": "HorseWithHats #9",
-"birthday": "10-18"
+"color": "brown"
 },
 "10": {
   "name": "HorseWithHats #10",
-"birthday": "10-18"
+"color": "brown"
 },
 "11": {
   "name": "HorseWithHats #11",
-"birthday": "10-18"
+"color": "brown"
 },
 "12": {
   "name": "HorseWithHats #12",
-"birthday": "10-18"
+"color": "brown"
 },
 "13": {
   "name": "HorseWithHats #13",
-"birthday": "10-18"
+"color": "brown"
 },
 "14": {
   "name": "HorseWithHats #14",
-"birthday": "10-18"
+"color": "brown"
 },
 "15": {
   "name": "HorseWithHats #15",
-"birthday": "10-18"
+"color": "brown"
 },
 "16": {
   "name": "HorseWithHats #16",
-"birthday": "10-18"
+"color": "brown"
 },
 "17": {
   "name": "HorseWithHats #17",
-"birthday": "10-18"
+"color": "brown"
 },
 "18": {
   "name": "HorseWithHats #18",
-"birthday": "10-18"
+"color": "brown"
 },
 "19": {
   "name": "HorseWithHats #19",
-"birthday": "10-18"
+"color": "brown"
 },
 "20": {
   "name": "HorseWithHats #20",
-"birthday": "10-18"
+"color": "brown"
 },
 "21": {
   "name": "HorseWithHats #21",
-"birthday": "10-18"
+"color": "brown"
 },
 "22": {
   "name": "HorseWithHats #22",
-"birthday": "10-18"
+"color": "brown"
 },
 "23": {
   "name": "HorseWithHats #23",
-"birthday": "10-18"
+"color": "brown"
 },
 "24": {
   "name": "HorseWithHats #24",
-"birthday": "10-18"
+"color": "brown"
+},
+"25": {
+  "name": "HorseWithHats #25",
+"color": "brown"
+},
+"26": {
+  "name": "HorseWithHats #26",
+"color": "brown"
+},
+"27": {
+  "name": "HorseWithHats #27",
+"color": "brown"
+},
+"28": {
+  "name": "HorseWithHats #28",
+"color": "brown"
+},
+"29": {
+  "name": "HorseWithHats #29",
+"color": "brown"
+},
+"30": {
+  "name": "HorseWithHats #30",
+"color": "brown"
+},
+"31": {
+  "name": "HorseWithHats #31",
+"color": "brown"
+},
+"32": {
+  "name": "HorseWithHats #32",
+"color": "brown"
+},
+"33": {
+  "name": "HorseWithHats #33",
+"color": "brown"
+},
+"34": {
+  "name": "HorseWithHats #34",
+"color": "brown"
+},
+"35": {
+  "name": "HorseWithHats #35",
+"color": "brown"
+},
+"36": {
+  "name": "HorseWithHats #36",
+"color": "brown"
+},
+"37": {
+  "name": "HorseWithHats #37",
+"color": "brown"
+},
+"38": {
+  "name": "HorseWithHats #38",
+"color": "brown"
+},
+"39": {
+  "name": "HorseWithHats #39",
+"color": "brown"
+},
+"40": {
+  "name": "HorseWithHats #40",
+"color": "brown"
+},
+"41": {
+  "name": "HorseWithHats #41",
+"color": "brown"
+},
+"42": {
+  "name": "HorseWithHats #42",
+"color": "brown"
+},
+"43": {
+  "name": "HorseWithHats #43",
+"color": "brown"
+},
+"44": {
+  "name": "HorseWithHats #44",
+"color": "brown"
+},
+"45": {
+  "name": "HorseWithHats #45",
+"color": "brown"
+},
+"46": {
+  "name": "HorseWithHats #46",
+"color": "brown"
+},
+"47": {
+  "name": "HorseWithHats #47",
+"color": "brown"
+},
+"48": {
+  "name": "HorseWithHats #48",
+"color": "brown"
+},
+"49": {
+  "name": "HorseWithHats #49",
+"color": "brown"
+},
+"50": {
+  "name": "HorseWithHats #50",
+"color": "brown"
+},
+"51": {
+  "name": "HorseWithHats #51",
+"color": "brown"
+},
+"52": {
+  "name": "HorseWithHats #52",
+"color": "brown"
+},
+"53": {
+  "name": "HorseWithHats #53",
+"color": "brown"
+},
+"54": {
+  "name": "HorseWithHats #54",
+"color": "brown"
+},
+"55": {
+  "name": "HorseWithHats #55",
+"color": "brown"
+},
+"56": {
+  "name": "HorseWithHats #56",
+"color": "brown"
+},
+"57": {
+  "name": "HorseWithHats #57",
+"color": "brown"
+},
+"58": {
+  "name": "HorseWithHats #58",
+"color": "brown"
+},
+"59": {
+  "name": "HorseWithHats #59",
+"color": "brown"
+},
+"60": {
+  "name": "HorseWithHats #60",
+"color": "brown"
+},
+"61": {
+  "name": "HorseWithHats #61",
+"color": "brown"
+},
+"62": {
+  "name": "HorseWithHats #62",
+"color": "brown"
+},
+"63": {
+  "name": "HorseWithHats #63",
+"color": "brown"
+},
+"64": {
+  "name": "HorseWithHats #64",
+"color": "brown"
+},
+"65": {
+  "name": "HorseWithHats #65",
+"color": "brown"
+},
+"66": {
+  "name": "HorseWithHats #66",
+"color": "brown"
+},
+"67": {
+  "name": "HorseWithHats #67",
+"color": "brown"
+},
+"68": {
+  "name": "HorseWithHats #68",
+"color": "brown"
+},
+"69": {
+  "name": "HorseWithHats #69",
+"color": "brown"
+},
+"70": {
+  "name": "HorseWithHats #70",
+"color": "brown"
+},
+"71": {
+  "name": "HorseWithHats #71",
+"color": "brown"
+},
+"72": {
+  "name": "HorseWithHats #72",
+"color": "brown"
+},
+"73": {
+  "name": "HorseWithHats #73",
+"color": "brown"
+},
+"74": {
+  "name": "HorseWithHats #74",
+"color": "brown"
+},
+"75": {
+  "name": "HorseWithHats #75",
+"color": "brown"
+},
+"76": {
+  "name": "HorseWithHats #76",
+"color": "brown"
+},
+"77": {
+  "name": "HorseWithHats #77",
+"color": "brown"
+},
+"78": {
+  "name": "HorseWithHats #78",
+"color": "brown"
+},
+"79": {
+  "name": "HorseWithHats #79",
+"color": "brown"
+},
+"80": {
+  "name": "HorseWithHats #80",
+"color": "brown"
+},
+"81": {
+  "name": "HorseWithHats #81",
+"color": "brown"
+},
+"82": {
+  "name": "HorseWithHats #82",
+"color": "brown"
+},
+"83": {
+  "name": "HorseWithHats #83",
+"color": "brown"
+},
+"84": {
+  "name": "HorseWithHats #84",
+"color": "brown"
+},
+"85": {
+  "name": "HorseWithHats #85",
+"color": "brown"
+},
+"86": {
+  "name": "HorseWithHats #86",
+"color": "brown"
+},
+"87": {
+  "name": "HorseWithHats #87",
+"color": "brown"
+},
+"88": {
+  "name": "HorseWithHats #88",
+"color": "brown"
+},
+"89": {
+  "name": "HorseWithHats #89",
+"color": "brown"
+},
+"90": {
+  "name": "HorseWithHats #90",
+"color": "brown"
+},
+"91": {
+  "name": "HorseWithHats #91",
+"color": "brown"
+},
+"92": {
+  "name": "HorseWithHats #92",
+"color": "brown"
+},
+"93": {
+  "name": "HorseWithHats #93",
+"color": "brown"
+},
+"94": {
+  "name": "HorseWithHats #94",
+"color": "brown"
+},
+"95": {
+  "name": "HorseWithHats #95",
+"color": "brown"
+},
+"96": {
+  "name": "HorseWithHats #96",
+"color": "brown"
+},
+"97": {
+  "name": "HorseWithHats #97",
+"color": "brown"
+},
+"98": {
+  "name": "HorseWithHats #98",
+"color": "brown"
+},
+"99": {
+  "name": "HorseWithHats #99",
+"color": "brown"
+},
+"100": {
+  "name": "HorseWithHats #100",
+"color": "brown"
+},
+"101": {
+  "name": "HorseWithHats #101",
+"color": "brown"
+},
+"102": {
+  "name": "HorseWithHats #102",
+"color": "brown"
+},
+"103": {
+  "name": "HorseWithHats #103",
+"color": "brown"
+},
+"104": {
+  "name": "HorseWithHats #104",
+"color": "brown"
+},
+"105": {
+  "name": "HorseWithHats #105",
+"color": "brown"
+},
+"106": {
+  "name": "HorseWithHats #106",
+"color": "brown"
+},
+"107": {
+  "name": "HorseWithHats #107",
+"color": "brown"
+},
+"108": {
+  "name": "HorseWithHats #108",
+"color": "brown"
+},
+"109": {
+  "name": "HorseWithHats #109",
+"color": "brown"
+},
+"110": {
+  "name": "HorseWithHats #110",
+"color": "brown"
+},
+"111": {
+  "name": "HorseWithHats #111",
+"color": "brown"
+},
+"112": {
+  "name": "HorseWithHats #112",
+"color": "brown"
+},
+"113": {
+  "name": "HorseWithHats #113",
+"color": "brown"
+},
+"114": {
+  "name": "HorseWithHats #114",
+"color": "brown"
+},
+"115": {
+  "name": "HorseWithHats #115",
+"color": "brown"
+},
+"116": {
+  "name": "HorseWithHats #116",
+"color": "brown"
+},
+"117": {
+  "name": "HorseWithHats #117",
+"color": "brown"
+},
+"118": {
+  "name": "HorseWithHats #118",
+"color": "brown"
+},
+"119": {
+  "name": "HorseWithHats #119",
+"color": "brown"
+},
+"120": {
+  "name": "HorseWithHats #120",
+"color": "brown"
+},
+"121": {
+  "name": "HorseWithHats #121",
+"color": "brown"
+},
+"122": {
+  "name": "HorseWithHats #122",
+"color": "brown"
+},
+"123": {
+  "name": "HorseWithHats #123",
+"color": "brown"
+},
+"124": {
+  "name": "HorseWithHats #124",
+"color": "brown"
+},
+"125": {
+  "name": "HorseWithHats #125",
+"color": "brown"
+},
+"126": {
+  "name": "HorseWithHats #126",
+"color": "brown"
+},
+"127": {
+  "name": "HorseWithHats #127",
+"color": "brown"
+},
+"128": {
+  "name": "HorseWithHats #128",
+"color": "brown"
+},
+"129": {
+  "name": "HorseWithHats #129",
+"color": "brown"
+},
+"130": {
+  "name": "HorseWithHats #130",
+"color": "brown"
+},
+"131": {
+  "name": "HorseWithHats #131",
+"color": "brown"
+},
+"132": {
+  "name": "HorseWithHats #132",
+"color": "brown"
+},
+"133": {
+  "name": "HorseWithHats #133",
+"color": "brown"
+},
+"134": {
+  "name": "HorseWithHats #134",
+"color": "brown"
+},
+"135": {
+  "name": "HorseWithHats #135",
+"color": "brown"
+},
+"136": {
+  "name": "HorseWithHats #136",
+"color": "brown"
+},
+"137": {
+  "name": "HorseWithHats #137",
+"color": "brown"
+},
+"138": {
+  "name": "HorseWithHats #138",
+"color": "brown"
+},
+"139": {
+  "name": "HorseWithHats #139",
+"color": "brown"
+},
+"140": {
+  "name": "HorseWithHats #140",
+"color": "brown"
+},
+"141": {
+  "name": "HorseWithHats #141",
+"color": "brown"
+},
+"142": {
+  "name": "HorseWithHats #142",
+"color": "brown"
+},
+"143": {
+  "name": "HorseWithHats #143",
+"color": "brown"
+},
+"144": {
+  "name": "HorseWithHats #144",
+"color": "brown"
+},
+"145": {
+  "name": "HorseWithHats #145",
+"color": "brown"
+},
+"146": {
+  "name": "HorseWithHats #146",
+"color": "brown"
+},
+"147": {
+  "name": "HorseWithHats #147",
+"color": "brown"
+},
+"148": {
+  "name": "HorseWithHats #148",
+"color": "brown"
+},
+"149": {
+  "name": "HorseWithHats #149",
+"color": "brown"
+},
+"150": {
+  "name": "HorseWithHats #150",
+"color": "brown"
+},
+"151": {
+  "name": "HorseWithHats #151",
+"color": "brown"
+},
+"152": {
+  "name": "HorseWithHats #152",
+"color": "brown"
+},
+"153": {
+  "name": "HorseWithHats #153",
+"color": "brown"
+},
+"154": {
+  "name": "HorseWithHats #154",
+"color": "brown"
+},
+"155": {
+  "name": "HorseWithHats #155",
+"color": "brown"
+},
+"156": {
+  "name": "HorseWithHats #156",
+"color": "brown"
+},
+"157": {
+  "name": "HorseWithHats #157",
+"color": "brown"
+},
+"158": {
+  "name": "HorseWithHats #158",
+"color": "brown"
+},
+"159": {
+  "name": "HorseWithHats #159",
+"color": "brown"
+},
+"160": {
+  "name": "HorseWithHats #160",
+"color": "brown"
+},
+"161": {
+  "name": "HorseWithHats #161",
+"color": "brown"
+},
+"162": {
+  "name": "HorseWithHats #162",
+"color": "brown"
+},
+"163": {
+  "name": "HorseWithHats #163",
+"color": "brown"
+},
+"164": {
+  "name": "HorseWithHats #164",
+"color": "brown"
+},
+"165": {
+  "name": "HorseWithHats #165",
+"color": "brown"
+},
+"166": {
+  "name": "HorseWithHats #166",
+"color": "brown"
+},
+"167": {
+  "name": "HorseWithHats #167",
+"color": "brown"
+},
+"168": {
+  "name": "HorseWithHats #168",
+"color": "brown"
+},
+"169": {
+  "name": "HorseWithHats #169",
+"color": "brown"
+},
+"170": {
+  "name": "HorseWithHats #170",
+"color": "brown"
+},
+"171": {
+  "name": "HorseWithHats #171",
+"color": "brown"
+},
+"172": {
+  "name": "HorseWithHats #172",
+"color": "brown"
+},
+"173": {
+  "name": "HorseWithHats #173",
+"color": "brown"
+},
+"174": {
+  "name": "HorseWithHats #174",
+"color": "brown"
+},
+"175": {
+  "name": "HorseWithHats #175",
+"color": "brown"
+},
+"176": {
+  "name": "HorseWithHats #176",
+"color": "brown"
+},
+"177": {
+  "name": "HorseWithHats #177",
+"color": "brown"
+},
+"178": {
+  "name": "HorseWithHats #178",
+"color": "brown"
+},
+"179": {
+  "name": "HorseWithHats #179",
+"color": "brown"
+},
+"180": {
+  "name": "HorseWithHats #180",
+"color": "brown"
+},
+"181": {
+  "name": "HorseWithHats #181",
+"color": "brown"
+},
+"182": {
+  "name": "HorseWithHats #182",
+"color": "brown"
+},
+"183": {
+  "name": "HorseWithHats #183",
+"color": "brown"
+},
+"184": {
+  "name": "HorseWithHats #184",
+"color": "brown"
+},
+"185": {
+  "name": "HorseWithHats #185",
+"color": "brown"
+},
+"186": {
+  "name": "HorseWithHats #186",
+"color": "brown"
+},
+"187": {
+  "name": "HorseWithHats #187",
+"color": "brown"
+},
+"188": {
+  "name": "HorseWithHats #188",
+"color": "brown"
+},
+"189": {
+  "name": "HorseWithHats #189",
+"color": "brown"
+},
+"190": {
+  "name": "HorseWithHats #190",
+"color": "brown"
+},
+"191": {
+  "name": "HorseWithHats #191",
+"color": "brown"
+},
+"192": {
+  "name": "HorseWithHats #192",
+"color": "brown"
+},
+"193": {
+  "name": "HorseWithHats #193",
+"color": "brown"
+},
+"194": {
+  "name": "HorseWithHats #194",
+"color": "brown"
+},
+"195": {
+  "name": "HorseWithHats #195",
+"color": "brown"
+},
+"196": {
+  "name": "HorseWithHats #196",
+"color": "brown"
+},
+"197": {
+  "name": "HorseWithHats #197",
+"color": "brown"
+},
+"198": {
+  "name": "HorseWithHats #198",
+"color": "brown"
+},
+"199": {
+  "name": "HorseWithHats #199",
+"color": "brown"
+},
+"200": {
+  "name": "HorseWithHats #200",
+"color": "brown"
+},
+"201": {
+  "name": "HorseWithHats #201",
+"color": "brown"
+},
+"202": {
+  "name": "HorseWithHats #202",
+"color": "brown"
+},
+"203": {
+  "name": "HorseWithHats #203",
+"color": "brown"
+},
+"204": {
+  "name": "HorseWithHats #204",
+"color": "brown"
+},
+"205": {
+  "name": "HorseWithHats #205",
+"color": "brown"
+},
+"206": {
+  "name": "HorseWithHats #206",
+"color": "brown"
+},
+"207": {
+  "name": "HorseWithHats #207",
+"color": "brown"
+},
+"208": {
+  "name": "HorseWithHats #208",
+"color": "brown"
+},
+"209": {
+  "name": "HorseWithHats #209",
+"color": "brown"
+},
+"210": {
+  "name": "HorseWithHats #210",
+"color": "brown"
+},
+"211": {
+  "name": "HorseWithHats #211",
+"color": "brown"
+},
+"212": {
+  "name": "HorseWithHats #212",
+"color": "brown"
+},
+"213": {
+  "name": "HorseWithHats #213",
+"color": "brown"
+},
+"214": {
+  "name": "HorseWithHats #214",
+"color": "brown"
+},
+"215": {
+  "name": "HorseWithHats #215",
+"color": "brown"
+},
+"216": {
+  "name": "HorseWithHats #216",
+"color": "brown"
+},
+"217": {
+  "name": "HorseWithHats #217",
+"color": "brown"
+},
+"218": {
+  "name": "HorseWithHats #218",
+"color": "brown"
+},
+"219": {
+  "name": "HorseWithHats #219",
+"color": "brown"
+},
+"220": {
+  "name": "HorseWithHats #220",
+"color": "brown"
+},
+"221": {
+  "name": "HorseWithHats #221",
+"color": "brown"
+},
+"222": {
+  "name": "HorseWithHats #222",
+"color": "brown"
+},
+"223": {
+  "name": "HorseWithHats #223",
+"color": "brown"
+},
+"224": {
+  "name": "HorseWithHats #224",
+"color": "brown"
+},
+"225": {
+  "name": "HorseWithHats #225",
+"color": "brown"
+},
+"226": {
+  "name": "HorseWithHats #226",
+"color": "brown"
+},
+"227": {
+  "name": "HorseWithHats #227",
+"color": "brown"
+},
+"228": {
+  "name": "HorseWithHats #228",
+"color": "brown"
+},
+"229": {
+  "name": "HorseWithHats #229",
+"color": "brown"
+},
+"230": {
+  "name": "HorseWithHats #230",
+"color": "brown"
+},
+"231": {
+  "name": "HorseWithHats #231",
+"color": "brown"
+},
+"232": {
+  "name": "HorseWithHats #232",
+"color": "brown"
+},
+"233": {
+  "name": "HorseWithHats #233",
+"color": "brown"
+},
+"234": {
+  "name": "HorseWithHats #234",
+"color": "brown"
+},
+"235": {
+  "name": "HorseWithHats #235",
+"color": "brown"
+},
+"236": {
+  "name": "HorseWithHats #236",
+"color": "brown"
+},
+"237": {
+  "name": "HorseWithHats #237",
+"color": "brown"
+},
+"238": {
+  "name": "HorseWithHats #238",
+"color": "brown"
+},
+"239": {
+  "name": "HorseWithHats #239",
+"color": "brown"
+},
+"240": {
+  "name": "HorseWithHats #240",
+"color": "brown"
+},
+"241": {
+  "name": "HorseWithHats #241",
+"color": "brown"
+},
+"242": {
+  "name": "HorseWithHats #242",
+"color": "brown"
+},
+"243": {
+  "name": "HorseWithHats #243",
+"color": "brown"
+},
+"244": {
+  "name": "HorseWithHats #244",
+"color": "brown"
+},
+"245": {
+  "name": "HorseWithHats #245",
+"color": "brown"
+},
+"246": {
+  "name": "HorseWithHats #246",
+"color": "brown"
+},
+"247": {
+  "name": "HorseWithHats #247",
+"color": "brown"
+},
+"248": {
+  "name": "HorseWithHats #248",
+"color": "brown"
+},
+"249": {
+  "name": "HorseWithHats #249",
+"color": "brown"
+},
+"250": {
+  "name": "HorseWithHats #250",
+"color": "brown"
+},
+"251": {
+  "name": "HorseWithHats #251",
+"color": "brown"
+},
+"252": {
+  "name": "HorseWithHats #252",
+"color": "brown"
+},
+"253": {
+  "name": "HorseWithHats #253",
+"color": "brown"
+},
+"254": {
+  "name": "HorseWithHats #254",
+"color": "brown"
+},
+"255": {
+  "name": "HorseWithHats #255",
+"color": "brown"
 }
 }
